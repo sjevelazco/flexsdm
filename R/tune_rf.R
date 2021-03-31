83f78a503d55e7fe6cb17ffcd4ff598dc72aa09f
Santiago Velazco <sjevelazco@gmail.com>
2021-03-31 12:44:53 -0300

    tune_rf created

---
+++ R/tune_rf.R
@@ -0,0 +1,190 @@
+#' Function for constructing Random Forest with exploration of hyper-parameters
+#'
+#'
+#'
+#' @param data 
+#' @param response 
+#' @param predictors 
+#' @param predictors_f 
+#' @param partition 
+#' @param grid 
+#' @param thr 
+#' @param metric 
+#'
+#' @importFrom dismo predict
+#' @importFrom dplyr bind_rows tibble select group_by_at summarise across everything pull
+#' @importFrom randomForest randomForest
+#' @importFrom stats formula
+#' 
+#' @return
+#' @export
+#'
+#' @examples
+tune_rf <-
+  function(data,
+           response,
+           predictors,
+           predictors_f = NULL,
+           partition,
+           grid = NULL,
+           thr = NULL,
+           metric = "TSS",
+           ...) {
+    
+    require(randomForest)
+    require(dplyr)
+    
+    data <- data.frame(data)
+    
+    # Transform response variable as factor
+    data[,response] <- as.factor(data[,response])
+    
+    if (is.null(predictors_f)) {
+      data <- data[, c(response, predictors, partition)]
+      data <- data.frame(data)
+    } else {
+      data <- data[, c(response, predictors, predictors_f, partition)]
+      data <- data.frame(data)
+      for (i in predictors_f) {
+        data[, i] <- as.factor(data[, i])
+      }
+    }
+    
+    # Formula
+    Fmula <- stats::formula(paste(response, '~', 
+                                  paste(c(predictors, predictors_f), collapse = " + ")))
+    
+    # Prepare grid when grid=default or NULL
+    if(is.null(grid)){
+      nv <- length(stats::na.omit(c(predictors, predictors_f)))
+      grid <- data.frame(mtry=round(sqrt(nv)))
+    } 
+    if(class(grid)=='character'){
+      nv <- length(stats::na.omit(c(predictors, predictors_f)))
+      if(grid=='defalut'){
+        grid <-  expand.grid(mtry = seq(2, nv, 1))
+        }
+    }
+    
+    # Test hyper-parameters names
+    hyperp <- names(grid)
+    if(!all(c("mtry")%in%hyperp)){
+      stop("Database used in 'grid' argument has to contain this columns for tunning: 'mtry'")
+    }
+    
+    grid$tune <- 1:nrow(grid)
+    
+    
+    N <- max(data[partition])
+    
+    train <- list()
+    test <- list()
+    for (i in 1:N) {
+      train[[i]] <- data[data[, partition] == i, ]
+      test[[i]] <- data[data[, partition] != i, ]
+    }
+    
+    eval_partial <- list()
+    for(i in 1:N){
+      message('Partition number: ', i, '/', N)
+      mod <- list()
+      for (ii in 1:nrow(grid)) {
+        set.seed(1)
+        try(mod[[ii]] <-
+              randomForest::randomForest(
+                Fmula,
+                data = train[[i]],
+                type = "classification",
+                mtry = grid$mtry[ii],
+                ntree = 500,
+                importance = FALSE
+              )
+        )
+      }
+      
+      filt <- !sapply(mod, is.null)
+      mod <- mod[filt]
+      grid2 <- grid[filt, ]
+      
+      
+      dismo::predict(
+        mod[[ii]],
+        newdata = test[[i]],
+        type = 'prob'
+      )
+      
+      # Predict for presences absences data
+      pred_test <-
+        lapply(mod, function(x)
+          data.frame(
+            pr_ab = test[[i]][,response],
+            pred = dismo::predict(
+              x,
+              newdata = test[[i]],
+              type = 'prob'
+            )[,2]
+          ))
+      
+      # Validation of parameter combination
+      eval <- list()
+      for (ii in 1:length(pred_test)) {
+        eval[[ii]] <-
+          enm_eval(p = pred_test[[ii]]$pred[pred_test[[ii]]$pr_ab == 1],
+                   a = pred_test[[ii]]$pred[pred_test[[ii]]$pr_ab == 0],
+                   thr = thr)
+      }
+      
+      eval <- dplyr::bind_rows(lapply(eval, function(x) x$selected_threshold))
+      eval <- dplyr::tibble(cbind(grid2, eval)) 
+      eval_partial[[i]] <- eval
+    }
+    
+    # Create final database with parameter performance
+    names(eval_partial) <- 1:N
+    eval_partial <- eval_partial %>% dplyr::bind_rows(.,.id='partition')
+    
+    eval_final <- eval_partial %>% 
+      dplyr::select(-partition, -c(tune:n_absences)) %>%
+      dplyr::group_by_at(hyperp) %>%
+      dplyr::summarise(dplyr::across(dplyr::everything(),
+                                     list(mean = mean, sd = sd)), .groups = "drop")
+    
+    # Find the bets parameter setting 
+    filt <- eval_final %>% dplyr::pull(paste0(metric, '_mean'))
+    filt <- which.max(filt)
+    best_tune <- eval_final[filt,]
+    best_hyperp <- eval_final[filt,hyperp]
+    
+    
+    # Fit final models with best settings 
+    set.seed(1)
+    mod <-
+      randomForest::randomForest(
+        Fmula,
+        data = data,
+        type = "classification",
+        mtry = best_hyperp$mtry,
+        ntree = 500,
+        importance = TRUE
+      )
+    
+    pred_test <- data.frame(
+      pr_ab = data[,response],
+      pred = dismo::predict(
+        mod,
+        newdata = data,
+        type = "prob"
+      )[,2])
+    
+    threshold <- enm_eval(p = pred_test$pred[pred_test$pr_ab == 1],
+                          a = pred_test$pred[pred_test$pr_ab == 0],
+                          thr = thr)
+    
+    result <- list(model = mod, 
+                   tune_performance=eval_final,
+                   best_hyper_performance=best_tune,
+                   best_hyper=best_hyperp,
+                   selected_threshold=threshold[[1]],
+                   threshold_table=threshold[[2]])
+    return(result)
+  }
