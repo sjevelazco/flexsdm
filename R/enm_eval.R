e673f5a69b258d110d751da052169a6852a7f3b1
Santiago Velazco <sjevelazco@gmail.com>
2021-04-05 15:12:28 -0300

    MAE metric added

--- R/enm_eval.R
+++ R/enm_eval.R
@@ -1,7 +1,7 @@
 #' Calculate different model performance metrics
 #'
-#' @description This function calculates threshold dependent and independent performance metric. It calculates TPR (True Positive Rate, also called sensitivity), TNR (True Negative Rate, , also called specificity), SORENSEN, JACCARD, FPB, OR (Omission Rate), TSS (True Skill Statistic) KAPPA, AUC (Area Under Curve), and BOYCE.
-#' @param p numeric. Predicted suitability for presences 
+#' @description This function calculates threshold dependent and independent performance metric. It calculates TPR (True Positive Rate, also called sensitivity), TNR (True Negative Rate, , also called specificity), SORENSEN, JACCARD, FPB, OR (Omission Rate), TSS (True Skill Statistic) KAPPA, AUC (Area Under Curve), BOYCE, MAE (Mean Absolute Error, values closer to zero denote better performance).
+#' @param p numeric. Predicted suitability for presences
 #' @param a numeric. Predicted suitability for presences absences
 #' @param thr character. Threshold used to get binary suitability values (i.e. 0,1). It is useful for threshold-dependent performance metrics. It is possible to use more than one threshold type. It is necessary to provide a vector for this argument. The next threshold area available:
 #' \itemize{
@@ -17,7 +17,7 @@
 #'   }
 #' In the case of use more than one threshold type it is necessary concatenate the names of threshold types, e.g., thr=c(type=c('LPT', 'MAX_TSS', 'MAX_JACCARD')). When SENSITIVITY threshold is used in combination with other it is necessary specify the desired sensitivity value, e.g. thr=c(type=c('LPT', 'MAX_TSS', 'SENSITIVITY'), sens='0.8')
 #'
-#' @param bg numeric. Predicted suitability for background points. It is used for BOYCE metric. It bg is set as null BOYCE metric will be calculated with presences and absences suitability values 
+#' @param bg numeric. Predicted suitability for background points. It is used for BOYCE metric. It bg is set as null BOYCE metric will be calculated with presences and absences suitability values
 #'
 #' @return a list with the next tibble
 #' \itemize{
@@ -28,50 +28,50 @@
 #' }
 #' when thr argument is NULL function will return a list with "performance", "threshold", and "threshold_table" tibbles, when thr argument is used with one or more threshold function returns "selected_threshold" and "threshold_table" tibbles
 #' @export
-#' 
+#'
 #' @importFrom dismo evaluate threshold
 #' @importFrom dplyr bind_cols left_join
-#' 
+#'
 #' @examples
 #' \dontrun{
 #' require(dismo)
 #' require(dplyr)
-#' 
+#'
 #' set.seed(0)
 #' p <- rnorm(50, mean=0.7, sd=0.3) %>% abs()
 #' p[p>1] <- 1
 #' p[p<0] <- 0
-#' 
+#'
 #' set.seed(0)
-#' a <- rnorm(50, mean=0.4, sd=0.4) %>% abs()
+#' a <- rnorm(50, mean=0.3, sd=0.2) %>% abs()
 #' a[a>1] <- 1
 #' a[a<0] <- 0
-#' 
+#'
 #' set.seed(0)
 #' backg <- rnorm(1000, mean=0.4, sd=0.4) %>% abs()
 #' backg[backg>1] <- 1
 #' backg[backg<0] <- 0
-#' 
-#' # Use function without threshold specification 
+#'
+#' # Use function without threshold specification
 #' e <- enm_eval(p, a)
 #' e$performance
 #' e$threshold
 #' e$threshold_table
-#' 
+#'
 #' enm_eval(p, a, thr=c(type=c('MAX_KAPPA')))
 #' enm_eval(p, a, thr=c(type=c('LPT', 'MAX_TSS', 'MAX_JACCARD')))
 #' enm_eval(p, a, thr=c(type=c('LPT', 'MAX_TSS', 'SENSITIVITY'))) # wrong way to SENSITIVITY threshold
 #' enm_eval(p, a, thr=c(type=c('LPT', 'MAX_TSS', 'SENSITIVITY'), sens='0.8')) # correct way to use SENSITIVITY threshold
-#' 
+#'
 #' # Use of bg argument (it will only be used for calculating BOYCE index)
 #' enm_eval(p, a, thr=c(type=c('MAX_TSS')))[[1]]
 #' enm_eval(p, a, thr=c(type=c('MAX_TSS')), bg=backg)[[1]]
-#' # I the case it is needed use background for calculate all other metric background values can be used in "a" argument 
+#' # I the case it is needed use background for calculate all other metric background values can be used in "a" argument
 #' enm_eval(p, backg, thr=c(type=c('MAX_TSS')))[[1]]
 #' }
-#' 
+#'
 enm_eval <- function(p, a, bg=NULL, thr=NULL){
-  
+
   if(any(
     !thr[grep('type', names(thr))] %in% c(
       "LPT",
@@ -86,22 +86,22 @@ enm_eval <- function(p, a, bg=NULL, thr=NULL){
   )) {
     stop("'thr' Argument is not valid!")
   }
-  
+
   if (any(thr[grep("type", names(thr))] %in% "SENSITIVITY") &&
       !any(names(thr) %in% "sens")) {
     stop(
       "provide a sensitivity value in the vector used in 'thr' argument, e.g. thr=c(type=c('LPT', 'MAX_TSS', 'SENSITIVITY'), sens='0.8')")
   }
-  
+
   np <- length(p)
   na <- length(a)
   if (na == 0 | np == 0) {
     stop('cannot evaluate a model without absence and presence data that are not NA')
   }
-  
+
   #Threshold breaks:
   # if(is.null(thr)){
-    
+
     if (length(p) > 1000) {
       tr <- as.vector(stats::quantile(p, 0:1000 / 1000))
     } else {
@@ -123,10 +123,10 @@ enm_eval <- function(p, a, bg=NULL, thr=NULL){
     eval_dismo <- dismo::evaluate(p=p, a=a, tr = tr)
   # }else{
   #   tr <- thr
-  #   
+  #
   #   eval_dismo <- dismo::evaluate(p=p, a=a, tr = tr)
   # }
-  
+
   res <- matrix(ncol = 4, nrow = length(tr))
   colnames(res) <- c('tp', 'fp', 'fn', 'tn')
   #Confusion Matrix
@@ -137,31 +137,31 @@ enm_eval <- function(p, a, bg=NULL, thr=NULL){
     res[i, 4] <- length(a[a < tr[i]])    # d  true negatives
   }
   res <- data.frame(res)
-  
+
   #Sorensen Index
   SOR <- 2 * res$tp / (res$fn + (2 * res$tp) + res$fp)
   # if(is.null(thr)){
-    SorTHR <- tr[which(SOR == max(SOR))][1]  
+    SorTHR <- tr[which(SOR == max(SOR))][1]
   # }else{
-  #   SorTHR <- tr  
+  #   SorTHR <- tr
   # }
-  
+
   #Jaccard Index
   JAC <- res$tp / (res$fn + res$tp + res$fp)
   # if(is.null(thr)){
     JacTHR <- tr[which(JAC == max(JAC))][1]
   # }else{
-  #   JacTHR <- tr  
+  #   JacTHR <- tr
   # }
-  
+
   #FPB
   FPB <- 2 * JAC
   # if(is.null(thr)){
     FpbTHR <- tr[which(FPB == max(FPB))][1]
   # }else{
-  #   FpbTHR <- tr  
+  #   FpbTHR <- tr
   # }
-  
+
   #TSS
   TPR <- res$tp / (res$tp + res$fn)
   TNR <- res$tn / (res$tn + res$fp)
@@ -169,14 +169,32 @@ enm_eval <- function(p, a, bg=NULL, thr=NULL){
   # if(is.null(thr)){
   TSSTHR <- tr[which(TSS == max(TSS))][1]
   # }else{
-  #   TSSTHR <- tr  
+  #   TSSTHR <- tr
   # }
+
+  # # SEDI
+  # Hi <- res$tp / (res$tp + res$fn) #true positive rate
+  # Fi <- res$fp / (res$tn + res$fp) #false positive rate
+  # Hi <- ifelse(Hi == 0, .Machine$double.eps, Hi)
+  # Fi <- ifelse(Fi == 0, .Machine$double.eps, Fi)
+  # Hi_minus1 <- 1 - Hi
+  # Fi_minus1 <- 1 - Fi
+  # Hi_minus1 <- ifelse(Hi_minus1 == 0, .Machine$double.eps, Hi_minus1)
+  # Fi_minus1 <- ifelse(Fi_minus1 == 0, .Machine$double.eps, Fi_minus1)
+  #
+  # SEDI <-
+  #   (log(Fi)-log(Hi)-log(Fi_minus1) + log(Hi_minus1)) /
+  #   (log(Fi)+log(Hi)+log(Fi_minus1) + log(Hi_minus1))
+  #
+  # # ORSS
+  # ORSS <- (res$tp*res$tn-res$fp*res$fn)/(res$tp*res$tn+res$fp*res$fn)
+
 
   thresholds <- list()
   thresholds$MAX_SORENSEN <- SorTHR
   thresholds$MAX_JACCARD <- JacTHR
   thresholds$MAX_FPB <- JacTHR
-  
+
   ThrDis <- c("kappa", "spec_sens", "no_omission", "equal_sens_spec", "sensitivity")
   ThrDis <- (sapply(ThrDis, function(x)
       dismo::threshold(eval_dismo)[x]))
@@ -186,7 +204,7 @@ enm_eval <- function(p, a, bg=NULL, thr=NULL){
   }
   thresholds <- c(ThrDis, thresholds)
   thresholds <- dplyr::bind_cols(thresholds)
-  
+
   #Final Result Object
   performance <- list()
   performance$threshold <- tr
@@ -198,37 +216,40 @@ enm_eval <- function(p, a, bg=NULL, thr=NULL){
   performance$JACCARD <- JAC
   performance$FPB <- FPB
   performance$OR  <- (1-performance$TPR)
-  performance$TSS <- (performance$TPR + performance$TNR) - 1
+  performance$TSS <- TSS
   performance$KAPPA <- eval_dismo@kappa
   R <- sum(rank(c(p, a))[1:np]) - (np * (np + 1)/2)
   performance$AUC <- R/(as.numeric(na) * as.numeric(np))
-  
+
   if(is.null(bg)){
     performance$BOYCE <- boyce(pres = p, contrast = c(p, a))
   } else {
     performance$BOYCE <- boyce(pres = p, contrast = c(p, bg))
   }
-  
+  real <- c(rep(1, length(p)), rep(0, length(a)))
+  pred <- c(p, a)
+  performance$MAE <- sum(abs(real-pred))/length(pred)
+
   performance <- dplyr::bind_cols(performance)
-  
+
   thr_table <- dplyr::tibble(threshold = names(thresholds), values=unlist(thresholds))
   thr_table <- dplyr::left_join(thr_table, performance, by=c('values'='threshold'))
-  
+
   #Return final result
-  
+
   result <-
     list(
       performance = performance,
       threshold = thresholds,
       threshold_table = thr_table
     )
-  
+
   if (is.null(thr)) {
     return(result)
   } else {
     result <- result$threshold_table
     result <-
-      list(selected_threshold = result[result$threshold %in% thr,], 
+      list(selected_threshold = result[result$threshold %in% thr,],
            threshold_table = result)
     return(result)
   }
