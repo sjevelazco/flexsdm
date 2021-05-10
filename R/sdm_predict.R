548b7702bbbb56134851ac7435375958861cee1b
Santiago Velazco <sjevelazco@gmail.com>
2021-05-10 11:25:19 -0300

    sdm_predict was updated to terra package

--- R/sdm_predict.R
+++ R/sdm_predict.R
@@ -3,53 +3,51 @@
 #' @description This function allows the prediction of one or more models built with the fit_ or tune_ function set. It can return continuous or continuous and binary predictions for one or more thresholds
 #'
 #' @param models list. A list a single or several models fitted with some of fit_ or tune_ functions
-#' @param pred stack or brick. Raster layer with predictor variables. Names of raster layers must exactly match those used in model fitting.
+#' @param pred SpatRaster. Raster layer with predictor variables. Names of layers must exactly match those used in model fitting.
 #' @param thr character. Binarize projection. Default NULL, i.e. function returns only continuous projection. If used with 'selected_thr', function returns continuous and binarized models used in the 'thr' argument of some of fit_ or tune_ functions. It used with "all_thr" , function returns continuous and binarized for all threshold types.
 #' @param calib_area SpatialPolygon or SpatialPolygonDataFrame. Spatial polygon used for restrinc prediction into a given region. Default = NULL
 #' @param clamp logical. It is set with TRUE, predictors and features are restricted to the range seen during model training. Only valid for Maxent model (see tune_mx and fit_mx)
 #' @param pred_type character. Type of response required available "link", "exponential", "cloglog" and "logistic". Default "cloglog". Only valid for Maxent model (see tune_mx and fit_mx)
 #' @return
 #'
-#' A list of Raster or RasterStack with continuous or continuous and binary predictions
+#' A list of SpatRaster with continuous or continuous and binary predictions
 #'
 #' @export
 #'
-#' @importFrom dplyr left_join mutate across
+#' @importFrom dplyr mutate across left_join
 #' @importFrom gam predict.Gam
 #' @importFrom GRaF predict.graf
 #' @importFrom kernlab predict
-#' @importFrom raster ncell as.data.frame values predict stack
-#' @importFrom stats na.exclude predict
+#' @importFrom stats predict
+#' @importFrom terra crop as.data.frame values rast
 #' @examples
 sdm_predict <- function(models, pred, thr, calib_area = NULL, clamp = TRUE, pred_type = "cloglog") {
 
   #### Prepare datasets ####
   # Detect factor predictors
-  fac_0 <- sapply(models, function(x) {
-    f <- x[[2]]
-    f <- f[(names(f) %>% grep('f', .))]
-    f
-  }) %>% unlist %>% unique
+  # fac_0 <- sapply(models, function(x) {
+  #   f <- x[[2]]
+  #   f <- f[(names(f) %>% grep('f', .))]
+  #   f
+  # }) %>% unlist %>% unique
 
   # Crop projection area
-  if(!is.null(calib_area)){
-    pred2 <- raster::mask(pred, calib_area)
-
+  if (!is.null(calib_area)) {
+    pred2 <- terra::crop(pred, calib_area)
     ### factor correction
-    if(length(fac_0)>0){
-      f <- lapply(fac_0, function(x) raster::ratify(pred[[x]])) %>% raster::stack()
-      for(ff in fac_0){
-        lev <- suppressMessages(dplyr::left_join(
-          raster::levels(f[[ff]])[[1]],
-          raster::levels(pred[[ff]])[[1]]))
-        levels(f[[ff]]) <- lev
-      }
-      pred <- raster::stack(raster::dropLayer(pred, fac_0), f)
-    } else {
-      pred <- pred2
-      rm(pred2)
-    }
-
+    # if(length(fac_0)>0){
+    #   f <- lapply(fac_0, function(x) raster::ratify(pred[[x]])) %>% raster::stack()
+    #   for(ff in fac_0){
+    #     lev <- suppressMessages(dplyr::left_join(
+    #       raster::levels(f[[ff]])[[1]],
+    #       raster::levels(pred[[ff]])[[1]]))
+    #     levels(f[[ff]]) <- lev
+    #   }
+    #   pred <- raster::stack(raster::dropLayer(pred, fac_0), f)
+    # } else {
+    #   pred <- pred2
+    #   rm(pred2)
+    # }
   }
 
 
@@ -67,29 +65,23 @@ sdm_predict <- function(models, pred, thr, calib_area = NULL, clamp = TRUE, pred
   names(model_c) <- names(m)
 
   ## Transform raster to data.frame
-  pred_df <- data.frame(
-    ncell = 1:raster::ncell(pred),
-    raster::as.data.frame(pred)
-  ) %>%
-    stats::na.exclude(pred_df)
-  rownames(pred_df) <- pred_df$ncell
-  pred_df$ncell <- NULL
+  pred_df <- terra::as.data.frame(pred, cells = FALSE, na.rm = TRUE)
 
   ### correct factor levels
-  if (length(fac_0) > 0) {
-    fac_1 <- names(pred_df)
-    fac_1 <- fac_1[!fac_1 %in% names(pred)]
-    fac_3 <- gsub("_category", "", fac_1)
-    for (i in 1:length(fac_3)) {
-      df1 <- pred[[fac_3[i]]]@data@attributes[[1]]
-      cc <- c("category")
-      names(cc) <- fac_1
-      pred_df[fac_1] <-
-        dplyr::left_join(pred_df[fac_1], df1, by = cc)[, 2]
-      names(pred_df)[names(pred_df) == fac_1] <- fac_3
-      pred_df[, fac_3] <- pred_df[, fac_3] %>% as.factor()
-    }
-  }
+  # if (length(fac_0) > 0) {
+  #   fac_1 <- names(pred_df)
+  #   fac_1 <- fac_1[!fac_1 %in% names(pred)]
+  #   fac_3 <- gsub("_category", "", fac_1)
+  #   for (i in 1:length(fac_3)) {
+  #     df1 <- pred[[fac_3[i]]]@data@attributes[[1]]
+  #     cc <- c("category")
+  #     names(cc) <- fac_1
+  #     pred_df[fac_1] <-
+  #       dplyr::left_join(pred_df[fac_1], df1, by = cc)[, 2]
+  #     names(pred_df)[names(pred_df) == fac_1] <- fac_3
+  #     pred_df[, fac_3] <- pred_df[, fac_3] %>% as.factor()
+  #   }
+  # }
 
 
 
@@ -113,7 +105,7 @@ sdm_predict <- function(models, pred, thr, calib_area = NULL, clamp = TRUE, pred
     wm <- names(wm)
     for (i in wm) {
       r <- pred[[1]]
-      raster::values(r) <- NA
+      terra::values(r) <- NA
 
       # Test factor levels
       f <- which(sapply(m[[i]]$data, class) == "factor")
@@ -158,7 +150,7 @@ sdm_predict <- function(models, pred, thr, calib_area = NULL, clamp = TRUE, pred
     wm <- names(wm)
     for (i in wm) {
       r <- pred[[1]]
-      raster::values(r) <- NA
+      terra::values(r) <- NA
 
       # Test factor levels
       f <- which(sapply(m[[i]]$data, class) == "factor")
@@ -203,7 +195,7 @@ sdm_predict <- function(models, pred, thr, calib_area = NULL, clamp = TRUE, pred
     wm <- names(wm)
     for (i in wm) {
       r <- pred[[1]]
-      raster::values(r) <- NA
+      terra::values(r) <- NA
       r[as.numeric(rownames(pred_df))] <-
         suppressMessages(stats::predict(m[[i]], pred_df, type = "response"))
 
@@ -218,7 +210,12 @@ sdm_predict <- function(models, pred, thr, calib_area = NULL, clamp = TRUE, pred
   if (length(wm) > 0) {
     wm <- names(wm)
     for (i in wm) {
-      model_c[[i]] <- raster::predict(pred, m[[i]], clamp = clamp, type = pred_type)
+      r <- pred[[1]]
+      terra::values(r) <- NA
+      r[as.numeric(rownames(pred_df))] <-
+        predict_maxnet(object = m[[i]], newdata = pred_df, clamp = clamp, type = pred_type)
+      model_c[[i]] <- r
+      rm(i)
     }
   }
 
@@ -228,7 +225,7 @@ sdm_predict <- function(models, pred, thr, calib_area = NULL, clamp = TRUE, pred
     wm <- names(wm)
     for (i in wm) {
       r <- pred[[1]]
-      raster::values(r) <- NA
+      terra::values(r) <- NA
 
       # Test factor levels
       f <- (m[[i]]$xlevels)
@@ -277,7 +274,7 @@ sdm_predict <- function(models, pred, thr, calib_area = NULL, clamp = TRUE, pred
     wm <- names(wm)
     for (i in wm) {
       r <- pred[[1]]
-      raster::values(r) <- NA
+      terra::values(r) <- NA
 
       # Test factor levels
       f <-
@@ -334,7 +331,7 @@ sdm_predict <- function(models, pred, thr, calib_area = NULL, clamp = TRUE, pred
     wm <- names(wm)
     for (i in wm) {
       r <- pred[[1]]
-      raster::values(r) <- NA
+      terra::values(r) <- NA
 
       # Test factor levels
       f_n <- which(sapply(pred_df, class) == "factor") %>% names()
@@ -414,10 +411,14 @@ sdm_predict <- function(models, pred, thr, calib_area = NULL, clamp = TRUE, pred
       model_b[[i]] <-
         lapply(thr_df[[i]]$values, function(x) {
           model_c[[i]] >= x
-        }) %>% raster::stack()
+        }) %>% terra::rast()
+      names(model_b[[i]]) <- names(thr_df[[i]]$values)
     }
     names(model_b) <- names(model_c)
-    result <- mapply(raster::stack, model_c, model_b, SIMPLIFY = FALSE)
+    mf <- function(x, x2) {
+      terra::rast(list(x, x2))
+    }
+    result <- mapply(mf, model_c, model_b, SIMPLIFY = FALSE)
     return(result)
   } else {
     result <- model_c
