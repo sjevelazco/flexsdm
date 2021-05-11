41739da54f775184ecfb597568074d20ca99024e
Santiago Velazco <sjevelazco@gmail.com>
2021-05-11 11:35:41 -0300

    homogenize_na updated and created

--- R/homogenize_na.R
+++ R/homogenize_na.R
@@ -1,20 +1,30 @@
-#' Title
+#' Homogenize cells with NAs across all layers
 #'
-#' @param x A stack or brick object.
+#' @param x A SpatRaster.
 #'
-#' @return
+#' @return a SpatRaster
 #' @export
 #'
+#' @importFrom terra mask
+#'
 #' @examples
+#' \dontrun{
+#' #' require(terra)
+#'
+#' somevar <- system.file("external/somevar.tif", package = "flexsdm")
+#' somevar <- terra::rast(somevar)
+#'
+#' somevar2 <- homogenize_na(somevar)
+#' par(mfrow=c(2,1))
+#' plot(somevar$CFP_4)
+#' plot(somevar2$CFP_4)
+#' par(mfrow=c(1,1))
+#'
+#' # In somevar2 all layers have the same cells with NAs
+#' }
 homogenize_na <- function(x) {
-  if (raster::canProcessInMemory(x, n = 2)) {
-    val <- raster::getValues(x)
-    NA.pos <- unique(which(is.na(val), arr.ind = T)[, 1])
-    val[NA.pos, ] <- NA
-    x <- raster::setValues(x, val)
-    return(x)
-  } else {
-    x <- raster::mask(x, calc(x, fun = sum))
-    return(x)
-  }
+  x2 <- sum(is.na(x))==0
+  x <- terra::mask(x, x2, maskvalues = 0)
+  rm(x2)
+  return(x)
 }
