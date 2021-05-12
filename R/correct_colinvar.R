7297f6f37db12dfc52c2a34f2db9944be00e2cee
Andre Felipe Alves de Andrade <andrefaandrade@gmail.com>
2021-05-12 17:14:07 -0300

    Fix to correct_colinvar

--- R/correct_colinvar.R
+++ R/correct_colinvar.R
@@ -14,10 +14,8 @@
 #' @return
 #' @export
 #'
-#' @importFrom nFactors parallel nScree
 #' @importFrom stats cor prcomp factanal
 #' @importFrom terra as.data.frame subset predict rast scale writeRaster
-#' @importFrom usdm vifstep
 #'
 #' @examples
 #' \dontrun{
@@ -25,74 +23,35 @@
 #' somevar <- terra::rast(somevar)
 #'
 #' #Perform pearson collinearity control
-#' h <- terra::as.data.frame(somevar)
-#' h <- base::abs(stats::cor(h, method = 'pearson'))
-#' diag(h) <- 0
-#'
-#' res <- as.list(1:10000)
-#' for(i in 1:10000){
-#'   ord <- base::sample(1:ncol(h))
-#'   h2 <- h[ord,ord]
-#'   h2[upper.tri(h2)] <- 0
-#'   res[[i]] <- names(somevar)[!apply(h2,2,function(x) any(x > 0.7))]
-#' }
-#'
-#' len <- sapply(res, function(x) length(x))
-#' sel <- res[[sample(which(len==max(len)),1)]]
-#' rem <- terra::names(rstack)[!names(rstack)%in%sel]
-#' rstack <- terra::subset(rstack,subset=sel)
-#' result <- list(
-#' rstack = rstack,
-#' removed_variables = rem,
-#' correlation_table = h
-#' )
+#' somevar <- correct_colinvar(rstack = somevar, method = c('pearson', th='0.8'))
+#' somevar$rstack
+#' somevar$removed_variables
+#' somevar$correlation_table
 #'
 #' #Perform vif collinearity control
-#' VF <- usdm::vifstep(terra::as.data.frame(somevar), th = 10)
-#' rem <- VF@excluded
-#' h <- VF@corMatrix
-#' diag(h) <- 0
-#' rstack <- terra::subset(rstack,subset=VF@variables[!VF@variables%in%VF@excluded])
-#' result <- list(
-#' rstack = rstack,
-#' removed_variables = rem,
-#' correlation_table = h
-#' )
+#' somevar <- correct_colinvar(rstack = somevar, method = c('vif', th='8'))
+#' somevar$rstack
+#' somevar$removed_variables
+#' somevar$correlation_table
 #'
 #' #Perform pca collinearity control
-#' p <- terra::as.data.frame(somevar,xy=FALSE,na.rm=TRUE)
-#' p <- stats::prcomp(p, retx = TRUE, scale. = TRUE, center = TRUE)
-#' means <- p$center
-#' stds <- p$scale
-#' cof <- p$rotation
-#' cvar <- summary(p)$importance["Cumulative Proportion", ]
-#' naxis <- Position(function(x) x>=0.95,cvar)
-#' cvar <- data.frame(cvar)
-#' rstack <- terra::predict(rstack,p,index=1:naxis)
-#' result <- list(
-#' rstack = rstack,
-#' coeficients = cof,
-#' cumulative_variance = cvar
-#' )
+#' somevar <- correct_colinvar(rstack = somevar, method = c('pca'))
+#' somevar$rstack
+#' somevar$coeficients
+#' somevar$cumulative_variance
+#'
+#' #Perform pca collinearity control for projections
+#' somevar <- correct_colinvar(rstack = somevar, method = c('pca', proj = ))
+#' somevar$rstack
+#' somevar$coeficients
+#' somevar$cumulative_variance
+#' somevar$proj
 #'
 #' #Perform fa collinearity control
-#' p <- terra::scale(rstack,center = TRUE, scale = TRUE)
-#' p <- terra::as.data.frame(p,xy=FALSE,na.rm=TRUE)
-#' e <- eigen(cor(p))
-#' ap <- nFactors::parallel(subject=nrow(p),var=ncol(p),rep=100,cent=.05)
-#' nS <- nFactors::nScree(x=e$values, aparallel=ap$eigen$qevpea)
-#' fit <- tryCatch(stats::factanal(x = p, factors = nS$Components$noc, rotation = "varimax", lower = 0.01),
-#' error = function(e) message("Warning: covariance matrix is not invertible. Consider removing highly correlated variables
-#' and trying again or choosing another method to control collinearity."))
-#' sel <- row.names(fit$loadings)[apply(fit$loadings,2,which.max)]
-#' rem <- row.names(fit$loadings)[!row.names(fit$loadings) %in% sel]
-#' rstack <- terra::subset(rstack,sel)
-#' result <- list(
-#' rstack = rstack,
-#' removed_variables = rem,
-#' correlation_table = fit$loadings
-#' )
-#'
+#' somevar <- correct_colinvar(rstack = somevar, method = c('fa'))
+#' somevar$rstack
+#' somevar$removed_variables
+#' somevar$correlation_table
 #' }
 #'
 correct_colinvar <- function(rstack,
@@ -125,7 +84,7 @@ correct_colinvar <- function(rstack,
       h2 <- h[ord, ord]
       h2[upper.tri(h2)] <- 0
       res[[i]] <-
-        names(rstack)[!apply(h2, 2, function(x)
+        colnames(h2)[!apply(h2, 2, function(x)
           any(x > th))]
     }
 
@@ -149,17 +108,50 @@ correct_colinvar <- function(rstack,
       th <- as.numeric(method['th'])
     }
 
-    VF <- usdm::vifstep(terra::as.data.frame(rstack), th = th)
-    rem <- VF@excluded
-    h <- VF@corMatrix
-    diag(h) <- 0
+    x <- terra::as.data.frame(rstack)
+    LOOP <- TRUE
+    if(nrow(x) > 10000){
+      x <- x[sample(1:nrow(x),10000),]
+    }
+    n <- list()
+    n$variables <- colnames(x)
+    exc <- c()
+
+    while (LOOP) {
+      v<-rep(NA,ncol(x))
+      names(v) <- colnames(x)
+      for (i in 1:ncol(x)) {
+        v[i] <-  1/(1-summary(lm(x[,i]~.,data=x[-i]))$r.squared)
+      }
+      if (v[which.max(v)] >= th) {
+        ex <- names(v[which.max(v)])
+        exc <- c(exc,ex)
+        x <- x[,-which(colnames(x) == ex)]
+      } else {
+        LOOP=FALSE
+      }
+    }
+    if (length(exc) > 0){
+      n$excluded <- exc
+    }
+
+    v<-rep(NA,ncol(x))
+    names(v) <- colnames(x)
+    for (i in 1:ncol(x)) {
+      v[i] <-  1/(1-summary(lm(x[,i]~.,data=x[-i]))$r.squared)
+    }
+
+    n$corMatrix <- cor(x, method="pearson")
+    n$results <- data.frame(Variables=names(v),VIF=as.vector(v))
+
+    diag(n$corMatrix) <- 0
     rstack <-
-      terra::subset(rstack, subset = VF@variables[!VF@variables %in% VF@excluded])
+      terra::subset(rstack, subset = n$results$Variables)
 
     result <- list(
       rstack = rstack,
-      removed_variables = rem,
-      correlation_table = h
+      removed_variables = n$excluded,
+      correlation_table = n$corMatrix
     )
   }
 
@@ -213,19 +205,26 @@ correct_colinvar <- function(rstack,
     }
   }
 
-  if (method %in% "fa") {
+  if (any(method %in% "fa")) {
     p <- terra::scale(rstack, center = TRUE, scale = TRUE)
     p <- terra::as.data.frame(p, xy = FALSE, na.rm = TRUE)
 
+    if(nrow(p) > 10000){
+      p <- p[sample(1:nrow(p),10000),]
+    }
+
     e <- eigen(cor(p))
-    ap <- nFactors::parallel(
-      subject = nrow(p),
-      var = ncol(p),
-      rep = 100,
-      cent = .05
-    )
-    nS <-
-      nFactors::nScree(x = e$values, aparallel = ap$eigen$qevpea)
+    len <- length(e$values)
+    a <- NULL
+    r <- NULL
+
+    for(j in 1:len){
+      a[j] <- 1/len * sum(1/(j:len) )
+      r[j] <- e$values[j]/(sum(e$values))
+    }
+
+    ns <- length(which(r > a))
+
     fit <-
       tryCatch(
         stats::factanal(
@@ -235,9 +234,9 @@ correct_colinvar <- function(rstack,
           lower = 0.01
         ),
         error = function(e)
-          message(
-            "Warning: covariance matrix is not invertible. Consider removing highly correlated variables and trying again or choosing another method to control collinearity."
-          )
+          stop(
+            "Covariance matrix is not invertible. Consider choosing another method to control collinearity.",
+            call. =F)
       )
 
     sel <-
