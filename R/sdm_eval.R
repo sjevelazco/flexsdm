faad8b671fc21f51c76556cc4340434b90bcf01f
Santiago Velazco <sjevelazco@gmail.com>
2021-05-25 09:39:51 -0300

    little bug corrected

--- R/sdm_eval.R
+++ R/sdm_eval.R
@@ -159,9 +159,9 @@ sdm_eval <- function(p, a, bg = NULL, thr = NULL) {
   performance <- performance %>% dplyr::mutate(AUC = R / (as.numeric(na) * as.numeric(np)))
 
   if (is.null(bg)) {
-    performance <- performance %>% dplyr::mutate(BOYCE = dplyr::mutate(pres = p, contrast = c(p, a)))
+    performance <- performance %>% dplyr::mutate(BOYCE = boyce(pres = p, contrast = c(p, a)))
   } else {
-    performance <- performance %>% dplyr::mutate(BOYCE = dplyr::mutate(pres = p, contrast = c(p, bg)))
+    performance <- performance %>% dplyr::mutate(BOYCE = boyce(pres = p, contrast = c(p, bg)))
   }
 
   real <- c(rep(1, length(p)), rep(0, length(a)))
