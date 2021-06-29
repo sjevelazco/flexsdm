e4fdcb22279eae143c4652ed38fbcf49c3bb46da
Brooke Rose <56370609+mrose048@users.noreply.github.com>
2021-06-29 13:34:54 -0700

    Update esm_glm.R
    grammar

--- R/esm_glm.R
+++ R/esm_glm.R
@@ -1,4 +1,4 @@
-#' Fit and validate Generalized Linear Models based Ensemble of Small of Model approach
+#' Fit and validate Generalized Linear Models based on Ensemble of Small of Model approach
 #'
 #' @description This function constructs Generalized Linear Models using the
 #' Ensemble of Small Model (ESM) approach (Breiner et al., 2015, 2018).
@@ -20,7 +20,7 @@
 #'   \item sensitivity: Threshold based on a specified sensitivity value.
 #'   Usage thr = c('sensitivity', sens='0.6') or thr = c('sensitivity'). 'sens' refers to sensitivity value. If it is not specified a sensitivity values, the function will use by default 0.9
 #'   }
-#' In the case of use more than one threshold type it is necessary concatenate threshold types, e.g., thr=c('max_sens_spec', 'max_jaccard'), or thr=c('max_sens_spec', 'sensitivity', sens='0.8'), or thr=c('max_sens_spec', 'sensitivity'). Function will use all thresholds if no threshold is specified
+#' If the user wants to include more than one threshold type, it is necessary to concatenate threshold types, e.g., thr=c('max_sens_spec', 'max_jaccard'), or thr=c('max_sens_spec', 'sensitivity', sens='0.8'), or thr=c('max_sens_spec', 'sensitivity'). Function will use all thresholds if no threshold is specified
 #' @param poly interger >= 2. If used with values >= 2 model will use polinomius
 #' for those continuous variables (i.e. used in predictors argument). Default is 0. Because ESM are
 #' constructed with few occurrences it would be recommended no use polynomials to avoid overfitting.
