4a7bc6aed1c1cd9abef96a326037b092b04a9c4a
Brooke Rose <56370609+mrose048@users.noreply.github.com>
2021-06-30 11:04:31 -0700

    Update fit_glm.R
    spelling

--- R/fit_glm.R
+++ R/fit_glm.R
@@ -80,7 +80,7 @@
 #'   predictors = c("aet", "ppt_jja", "pH", "awc", "depth"),
 #'   predictors_f = c("landform"),
 #'   partition = ".part",
-#'   thr = c("max_sens_spec", "equal_sens_spec", "mas_sorensen"),
+#'   thr = c("max_sens_spec", "equal_sens_spec", "max_sorensen"),
 #'   poly = 2,
 #'   inter_order = 1
 #' )
