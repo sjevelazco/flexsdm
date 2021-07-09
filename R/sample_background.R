9c57cfc781fa1345e798e4e72148c6ed1ed7a6dd
Santiago Velazco <sjevelazco@gmail.com>
2021-07-09 12:51:09 -0300

    a new background method was documented and website updated

--- R/sample_background.R
+++ R/sample_background.R
@@ -7,10 +7,12 @@
 #' @param x character. Column name with longitude data
 #' @param y character. Column name with latitude data
 #' @param n integer. Number of background point to be sampled
+#' @param method character. Background allocation method. The methods implemented are:
 #' \itemize{
 #' \item random: Random allocation of background points. Usage method = 'random'.
-#' \item thickening: Thickening background point based on Vollering et al. (2019) method. For method, it is necessary to define a buffer width that will be used around presences points. It possible to define a buffer using the argument as method = c("thickening", width = 20000). Buffer width must be in m if raster (used in rlayer) has a longitude/latitude CRS, or map units in other cases. If a width buffer is not provided function will use a width value equal to the mean distance of the pair-wise presences distance. In case of a width value is not provided, the argument must be used as method='thickening'. Usage method='thickening' or method = c("thickening", width = 20000).
+#' \item thickening: Thickening background point based on Vollering et al. (2019) method. For method, it is necessary to define a buffer width that will be used around presences points. It possible to define a buffer using the argument as method = c("thickening", width = 20000). Buffer width must be in m if raster (used in rlayer) has a longitude/latitude CRS, or map units in other cases. If a width buffer is not provided function will use a width value equal to the mean distance of the pair-wise presences distance. In case of a width value is not provided, the argument must be used as method='thickening'.
 #' }
+#' Usage method='thickening' or method = c("thickening", width = 20000). Default 'random'
 #' @param rlayer SpatRaster used for sampling background points.
 #' It is recommended to use a layer with the same resolution and extent that environmental variables that will be used for modeling. In the case use maskval argument, this raster layer must contain the values to sampling constraint
 #' @param maskval integer or numeric. Values of the raster layer used for constraining the background points sampling
