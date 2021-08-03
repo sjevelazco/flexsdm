a73691c6a147fd77f40fd497aa2ef33e6d549bd5
Santiago Velazco <sjevelazco@gmail.com>
2021-08-03 08:15:20 -0300

    deleted tests were added

---
+++ tests/testthat/test-fit_glm.R
@@ -0,0 +1,102 @@
+test_that("multiplication works", {
+  data("abies")
+
+  # Using k-fold partition method
+  abies2 <- part_random(
+    data = abies,
+    pr_ab = "pr_ab",
+    method = c(method = "kfold", folds = 3)
+  )
+
+  glm_t1 <- fit_glm(
+    data = abies2,
+    response = "pr_ab",
+    predictors = c("aet", "ppt_jja", "pH", "awc", "depth"),
+    predictors_f = c("landform"),
+    partition = ".part",
+    thr = c("max_sens_spec", "equal_sens_spec", "max_sorensen"),
+    poly = 0,
+    inter_order = 0
+  )
+
+  expect_equal(class(glm_t1), "list")
+
+  # testing with polynomial and interaction term
+  glm_t2 <- fit_glm(
+    data = abies2,
+    response = "pr_ab",
+    predictors = c("aet", "ppt_jja", "pH", "awc", "depth"),
+    predictors_f = c("landform"),
+    partition = ".part",
+    thr = c("max_sens_spec", "equal_sens_spec", "max_sorensen"),
+    poly = 2,
+    inter_order = 1
+  )
+
+  expect_equal(class(glm_t2), "list")
+
+  # Using repeated k-fold partition method
+  abies2 <- part_random(
+    data = abies,
+    pr_ab = "pr_ab",
+    method = c(method = "rep_kfold", folds = 3, replicates = 3)
+  )
+
+  glm_t3 <- fit_glm(
+    data = abies2,
+    response = "pr_ab",
+    predictors = c("ppt_jja", "pH", "awc"),
+    predictors_f = c("landform"),
+    partition = ".part",
+    thr = c("max_sens_spec", "equal_sens_spec", "max_sorensen"),
+    poly = 3,
+    inter_order = 2
+  )
+  expect_equal(class(glm_t3), "list")
+
+
+  # Does the function work without predictors_f?
+  glm_t3 <- fit_glm(
+    data = abies2,
+    response = "pr_ab",
+    predictors = c("aet", "ppt_jja", "pH", "awc", "depth"),
+    partition = ".part",
+    thr = c("max_sens_spec", "equal_sens_spec", "max_sorensen")
+  )
+
+  expect_equal(class(glm_t3), "list")
+
+  # What about no predictors? Does not work
+  expect_error(fit_glm(
+    data = abies2,
+    response = "pr_ab",
+    predictors_f = c("landform"),
+    partition = ".part",
+    thr = c("max_sens_spec", "equal_sens_spec", "max_sorensen")
+  ))
+})
+
+
+test_that("test glm with NA, no factor variable and using formula", {
+  data("abies")
+
+  # Using k-fold partition method
+  abies2 <- part_random(
+    data = abies,
+    pr_ab = "pr_ab",
+    method = c(method = "kfold", folds = 3)
+  )
+
+  abies2$aet[2:10] <- NA
+  glm_t1 <- fit_glm(
+    data = abies2,
+    response = "pr_ab",
+    predictors = c("aet", "ppt_jja", "pH", "awc", "depth"),
+    predictors_f = NULL,
+    partition = ".part",
+    thr = c("max_sens_spec", "equal_sens_spec", "max_sorensen"),
+    fit_formula = as.formula("pr_ab ~ aet + ppt_jja + pH + awc + depth")
+  )
+
+  expect_equal(class(glm_t1), "list")
+})
---
+++ tests/testthat/test-tune_gbm.R
@@ -0,0 +1,248 @@
+## %######################################################%##
+#                                                          #
+####          Set of tests for testing errors           ####
+#                                                          #
+## %######################################################%##
+
+test_that("class and lenght of gbm_t object", {
+  data(abies)
+  abies
+
+  # We will partition the data with the k-fold method
+
+  abies2 <- part_random(
+    data = abies %>% dplyr::group_by(pr_ab) %>%
+      dplyr::slice_sample(prop = .2),
+    pr_ab = "pr_ab",
+    method = c(method = "kfold", folds = 2)
+  )
+
+  # Hyper-parameter values for tuning
+  tune_grid <-
+    expand.grid(
+      n.trees = c(20, 50),
+      shrinkage = c(0.1, 0.5),
+      n.minobsinnode = c(1, 3)
+    )
+
+  gbm_t <-
+    tune_gbm(
+      data = abies2,
+      response = "pr_ab",
+      predictors = c(
+        "aet", "cwd", "awc", "depth"
+      ),
+      predictors_f = c("landform"),
+      partition = ".part",
+      grid = tune_grid,
+      thr = "max_sens_spec",
+      metric = "TSS"
+    )
+
+  expect_equal(class(gbm_t), "list")
+  expect_equal(length(gbm_t), 5)
+})
+
+test_that("test of 0-1 response argument", {
+  data(abies)
+
+  # We will partition the data with the k-fold method
+  abies2 <- part_random(
+    data = abies %>% dplyr::group_by(pr_ab) %>%
+      dplyr::slice_sample(prop = .2),
+    pr_ab = "pr_ab",
+    method = c(method = "kfold", folds = 2)
+  )
+
+  # Hyper-parameter values for tuning
+  tune_grid <-
+    expand.grid(
+      n.trees = c(20, 50),
+      shrinkage = c(0.1, 0.5),
+      n.minobsinnode = c(1, 3)
+    )
+
+  expect_error(gbm_t <-
+    tune_gbm(
+      data = abies2,
+      response = "aet",
+      predictors = c(
+        "aet", "cwd", "awc", "depth"
+      ),
+      predictors_f = c("landform"),
+      partition = ".part",
+      grid = tune_grid,
+      thr = "max_sens_spec",
+      metric = "TSS"
+    ))
+})
+
+test_that("test NULL in predictors_f", {
+  data(abies)
+
+  abies2 <- part_random(
+    data = abies %>% dplyr::group_by(pr_ab) %>%
+      dplyr::slice_sample(prop = .2),
+    pr_ab = "pr_ab",
+    method = c(method = "kfold", folds = 2)
+  )
+
+  tune_grid <-
+    expand.grid(
+      n.trees = c(20, 50),
+      shrinkage = c(0.1, 0.5),
+      n.minobsinnode = c(1, 3)
+    )
+
+  gbm_t <-
+    tune_gbm(
+      data = abies2,
+      response = "pr_ab",
+      predictors = c(
+        "aet", "cwd", "tmin", "ppt_djf", "ppt_jja",
+        "ppt_jja", "pH", "awc", "depth"
+      ),
+      predictors_f = NULL,
+      partition = ".part",
+      grid = tune_grid,
+      thr = "max_sens_spec",
+      metric = "TSS"
+    )
+
+  expect_equal(class(gbm_t), "list")
+  expect_equal(length(gbm_t), 5)
+})
+
+test_that("test if remove NAs rows works", {
+  data(abies)
+
+  # We will partition the data with the k-fold method
+  abies2 <- part_random(
+    data = abies %>% dplyr::group_by(pr_ab) %>%
+      dplyr::slice_sample(prop = .2),
+    pr_ab = "pr_ab",
+    method = c(method = "kfold", folds = 2)
+  )
+
+  # Hyper-parameter values for tuning
+  tune_grid <-
+    expand.grid(
+      n.trees = c(20, 50),
+      shrinkage = c(0.1, 0.5),
+      n.minobsinnode = c(1, 3)
+    )
+
+  # Insert NAs in rows 3 and 4 for response column.
+  abies2[3:4, 1] <- NA
+
+  expect_message(gbm_t <-
+    tune_gbm(
+      data = abies2,
+      response = "pr_ab",
+      predictors = c(
+        "aet", "cwd", "tmin", "ppt_djf", "ppt_jja",
+        "ppt_jja", "pH", "awc", "depth"
+      ),
+      predictors_f = c("landform"),
+      partition = ".part",
+      grid = tune_grid,
+      thr = "max_sens_spec",
+      metric = "TSS"
+    ))
+
+  # Compare if the 2 NAs were removed
+  testthat:::compare.numeric(nrow(abies2), nrow(gbm_t$data_ens))
+})
+
+test_that("test fit_formula", {
+  data(abies)
+
+  abies2 <- part_random(
+    data = abies %>% dplyr::group_by(pr_ab) %>%
+      dplyr::slice_sample(prop = .2),
+    pr_ab = "pr_ab",
+    method = c(method = "kfold", folds = 2)
+  )
+
+  # Hyper-parameter values for tuning
+  tune_grid <-
+    expand.grid(
+      n.trees = c(20, 50),
+      shrinkage = c(0.1, 0.5),
+      n.minobsinnode = c(1, 3)
+    )
+
+  expect_message(gbm_t <-
+    tune_gbm(
+      data = abies2,
+      response = "pr_ab",
+      predictors = c(
+        "aet", "ppt_jja", "depth"
+      ),
+      predictors_f = c("landform"),
+      fit_formula = formula("pr_ab ~ aet + ppt_jja + depth + landform"),
+      partition = ".part",
+      grid = tune_grid,
+      thr = "max_sens_spec",
+      metric = "TSS"
+    ))
+})
+
+test_that("grid = NULL ", {
+  data(abies)
+
+  abies2 <- part_random(
+    data = abies %>% dplyr::group_by(pr_ab) %>%
+      dplyr::slice_sample(prop = .2),
+    pr_ab = "pr_ab",
+    method = c(method = "kfold", folds = 2)
+  )
+
+  expect_message(gbm_t <-
+    tune_gbm(
+      data = abies2,
+      response = "pr_ab",
+      predictors = c(
+        "aet", "awc", "depth"
+      ),
+      predictors_f = c("landform"),
+      partition = ".part",
+      grid = NULL,
+      thr = "max_sens_spec",
+      metric = "TSS"
+    ))
+})
+
+
+test_that("missuse of grid ", {
+  data(abies)
+
+  abies2 <- part_random(
+    data = abies %>% dplyr::group_by(pr_ab) %>%
+      dplyr::slice_sample(prop = .2),
+    pr_ab = "pr_ab",
+    method = c(method = "kfold", folds = 2)
+  )
+
+  # Hyper-parameter values for tuning
+  tune_grid <-
+    expand.grid(
+      n.trees = c(20, 50),
+      # shrinkage = c(0.1, 0.5),
+      n.minobsinnode = c(1, 3)
+    )
+
+  expect_error(gbm_t <-
+    tune_gbm(
+      data = abies2,
+      response = "pr_ab",
+      predictors = c(
+        "aet", "awc", "depth"
+      ),
+      predictors_f = c("landform"),
+      partition = ".part",
+      grid = grid,
+      thr = "max_sens_spec",
+      metric = "TSS"
+    ))
+})
