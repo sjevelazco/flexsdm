dc4ffd3af2fac39694e32f39dc83c0b333da440a
sjevelazco <sjevelazco@gmail.com>
2023-06-30 07:53:58 -0300

    web site was updated

--- tests/testthat/test-p_extra.R
+++ tests/testthat/test-p_extra.R
@@ -41,6 +41,7 @@ test_that("test p_extra", {
   extr <-
     extra_eval(
       training_data = sp_pa_2, # change by training_data
+      pr_ab = "pr_ab",
       projection_data = somevar, # change to projection_data
       n_cores = 1,
       aggreg_factor = 1
